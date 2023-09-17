f5ff1dbf4ccd4dd2ce354e2e04f484f834efe8f4
HaiderAli00033 <minestudy142@gmail.com>
2023-09-17 05:15:00 -0700

    Changed

--- script.js
+++ script.js
@@ -1,58 +1,15 @@
-// let name = prompt("Enter Your Name");
-// let cname = prompt("Enter Your Crush name");
+let menu = document.querySelector(".menu");
+let nav = document.querySelector(".nav-mob");
+let navpc = document.querySelector(".nav-main")
+let close = document.querySelector(".end")
 
+menu.addEventListener("click", function(){
+  nav.classList.toggle("show");
+  navpc.classList.add('remove');
+  menu.classList.add('pressed')
+})
 
-
-// let random = Math.random();
-// let b = Math.floor(random * 100)+1;
-
-
-// alert("This is your love Percentage " + b + "%");
-
-
-
-    // let name = prompt("Enter Your Name");
-    // let cname = prompt("Enter Your Crush's Name");
-  
-    // let random = Math.random();
-    // let lovePercentage = Math.floor(random * 100) + 1;
-
-    // if (lovePercentage > 70) {
-    // alert("Your Love Percentage with " + cname + " is " + lovePercentage + "%. You love eachother like kenya love kanye");
-    // }else{
-    //     alert("Your Love Percentage with " + cname + " is " + lovePercentage + "%");
-    // }
-
-
-        // function BMICal(weight, height) {
-    //     let BMI = weight / (height * height);
-    //     let interpretation;
-      
-    //     if (BMI > 24.9) {
-    //       interpretation = "Your BMI is " + BMI + ". You are overweighted.";
-    //     } else if (BMI >= 18.5) {
-    //       interpretation = "Your BMI is " + BMI + ". Your weight is normal.";
-    //     } else {
-    //       interpretation = "Your BMI is " + BMI + ". You are underweight.";
-    //     }
-      
-    //     return interpretation;
-    //   }
-
-function isLeap(year) {
-  if (year % 4 == 0) {
-    if (year % 100 == 0){
-      if (year % 400 == 0 ){
-        console.log("Leap Year");
-      }else {
-        console.log("Not Leap Year");
-      }
-    }else {
-      console.log("Leap Year");
-    }
-  }else {
-    console.log("Not Leap Year");
-  }
-}
-
-isLeap(2028);
+close.addEventListener("click", function () {
+  nav.classList.toggle('show');
+  navpc.classList.remove('remove');
+})
