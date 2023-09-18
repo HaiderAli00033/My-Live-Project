3a801c2e211657b0feed362e796d9a71dda7a8ec
HaiderAli00033 <minestudy142@gmail.com>
2023-09-17 18:11:50 -0700

    Fixes

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ let close = document.querySelector(".end")
 menu.addEventListener("click", function(){
   nav.classList.toggle("show");
   navpc.classList.add('remove');
-  menu.classList.add('pressed')
 })
 
 close.addEventListener("click", function () {
   nav.classList.toggle('show');
   navpc.classList.remove('remove');
+})
+
+window.addEventListener("resize", function(){
+  nav.classList.remove("show");
+  navpc.classList.remove("remove");
 })
